c1ef12a3fddc01521207f43c342db70e964da41e
Maxime <maxime.cougourdan@laposte.net>
2022-10-11 16:02:53 +0200

    navbar fixe et animations

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ jQuery(function(){
         });
     });
 });
+
+const ratio =.01
+var options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+};
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      console.log('visible');
+      entry.target.classList.add('reveal-visible');
+      observer.unobserve(entry.target);
+    }
+  });
+
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function(r) {
+      observer.observe(r);
+});
