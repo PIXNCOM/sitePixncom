bb8b932d4303cc316c2ba0780f9a75b93e101470
Maxime <maxime.cougourdan@laposte.net>
2022-10-26 13:47:17 +0200

    Formulaire + captcha

--- traitement.php
+++ traitement.php
@@ -1,56 +1,81 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
 
-// On vérifie que la méthode POST est utilisée
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    // On vérifie si le champ "recaptcha-response" contient une valeur
-    if(empty($_POST['recaptcha-response'])){
-        header('Location: index.php');
-    }else{
-        // On prépare l'URL
-        $url = "https://www.google.com/recaptcha/api/siteverify?secret=6LcnNK8iAAAAADqPD_lCRIAF7BLQLk5A5FLRLud0&response={$_POST['recaptcha-response']}";
+//Pour utiliser notre objet PHPMailer
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-        // On vérifie si curl est installé
-        if(function_exists('curl_version')){
-            $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_HEADER, false);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_TIMEOUT, 1);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            $response = curl_exec($curl);
-        }else{
-            // On utilisera file_get_contents
-            $response = file_get_contents($url);
-        }
 
-        // On vérifie qu'on a une réponse
-        if(empty($response) || is_null($response)){
-            header('Location: index.php');
-        }else{
-            $data = json_decode($response);
-            if($data->success){
-                if(
-                    isset($_POST['nom']) && !empty($_POST['nom']) &&
-                    isset($_POST['telephone']) && !empty($_POST['telephone']) &&
-                    isset($_POST['email']) && !empty($_POST['email']) &&
-                    isset($_POST['message']) && !empty($_POST['message'])
-                ){
-                    // On nettoie le contenu
-                    $nom = strip_tags($_POST['nom']);
-                    $telephone = strip_tags($_POST['telephone']);
-                    $email = strip_tags($_POST['email']);
-                    $message = htmlspecialchars($_POST['message']);
+$nom;$telephone;$email;$message;$captcha;
+$nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
+$telephone = filter_input(INPUT_POST, 'telephone', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+$captcha = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
+ if(!$captcha){
+   echo '
+   Please check the the captcha form.
+   ';
+   exit;
+ }
+ $secretKey = "6LcnNK8iAAAAADqPD_lCRIAF7BLQLk5A5FLRLud0";
+ $ip = $_SERVER['REMOTE_ADDR'];
 
-                    // Ici vous traitez vos données
+ // post request to server
+ $url = 'https://www.google.com/recaptcha/api/siteverify';
+ $data = array('secret' => $secretKey, 'response' => $captcha);
 
-                    echo "Message de {$nom} envoyé";
-                }
-            }else{
-                header('Location: index.php');
-            }
-        }
-    }
-}else{
-    http_response_code(405);
-    echo 'Méthode non autorisée';
-}
-?>
+ $options = array(
+   'http' => array(
+     'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+     'method'  => 'POST',
+     'content' => http_build_query($data)
+   )
+ );
+ $context  = stream_context_create($options);
+ $response = file_get_contents($url, false, $context);
+ $responseKeys = json_decode($response,true);
+ header('Content-type: application/json');
+ if($responseKeys["success"]) {
+
+
+   $mail = new PHPMailer(true);
+   try {
+     $mail->isSMTP(); //Pour preciser que c'est du SMTP
+     $mail->Host = 'smtp.gmail.com';  // Le serveur smtp de google
+     $mail->SMTPAuth = true;                               // On active l'authentification
+     $mail->Username = 'pixncom@gmail.com';                 // SMTP username
+     $mail->Password = 'rngrwsjxomdvlubt';                           // Le mot de passe que vous avez récupéré
+     $mail->SMTPSecure = 'tls';                            // Parameter de sécurité mis sur TLS
+     $mail->Port = 587;                                    // Le port donne par google pour son SMTP
+     $mail->addAddress('dev3@pixncom.com', 'pixncom'); //La boite mail où vous voulez recevoir les mails
+     $Body = "Nom: " . $nom . "\n";
+
+     $Body .= "Email: " . $email . "\n";
+
+     $Body .= "Telephone: " . $telephone . "\n";
+
+     $Body .= "Message: " . $message . "\n";
+
+     $mail->isHTML(true); //Met le mail au format HTML
+     $mail->Subject = "formulaire pixncom"; // On parametre l'objet
+     $mail->Body = nl2br($Body); // Le message pour les boites html
+     $mail->AltBody = $Body; //Le message pour les boites non html
+     $mail->SMTPDebug = 0; //On désactive les logs de debug
+
+
+
+     if(!$mail->send()) {
+       echo $mail->ErrorInfo;
+     } else{
+       echo json_encode(array('success' => 'true'));
+     }
+   } catch (Exception $e) {
+     $erreur = $e;
+   }
+ } else {
+   echo json_encode(array('success' => 'false'));
+ }
+ ?>
